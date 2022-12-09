811108f0b5dfd0c4dd8ab86a7864cd0bd88f5100
yuheng <sc20yl2@leeds.ac.uk>
2022-12-09 23:34:25 +0800

    fix a bug

--- blueprints/camp.py
+++ blueprints/camp.py
@@ -1,14 +1,31 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, g, session, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for, flash, g, session, jsonify, Response
 from flask_mail import Message
+from flask_restful import Resource, Api
+import json
+from models import CampModel, CampUserModel
 
 
 # the information of the blueprint
 bp = Blueprint("camp", __name__, url_prefix="/camp")
+api = Api(bp)
 
 
-@bp.route("/", methods=["GET", "POST"])
-def camp():
-    return render_template("camp.html")
+@api.representation('text/html')
+def output_html(data, code, headers=None):
+    if isinstance(data, str):
+        resp = Response(data)
+        return resp
+    else:
+        return Response(json.dumps(data), mimetype='application/json')
+
+
+class Camp(Resource):
+    def get(self, camp_id):
+
+        return render_template("camp.html")
+
+
+api.add_resource(Camp, "/<int:camp_id>")
 
 
 @bp.route("/post", methods=["GET", "POST"])
