71593900552c7ad5f4f3ad539756f4b9a138655c
yuheng <sc20yl2@leeds.ac.uk>
2022-12-14 22:31:34 +0800

    cookie informs

--- static/script/index.js
+++ static/script/index.js
@@ -1,4 +1,4 @@
-function join_camp(camp_id){
+function join_camp(camp_id) {
     // using ajax to send the request
     $.ajax({
         url: "/join_camp",
@@ -11,14 +11,30 @@ function join_camp(camp_id){
             if (code === 200) {
                 layer.msg(res['message']);
                 // change the dom '#users_num' to the new number of users
-                $('#users_num_'+camp_id).text(res['users_num']);
+                $('#users_num_' + camp_id).text(res['users_num']);
                 // wait 1 second and redirect to the camp page
                 setTimeout(function () {
                     window.location.href = "/camp/" + res["camp_id"];
-                } , 1000);
+                }, 1000);
             } else {
                 layer.msg(res['message']);
             }
         }
     })
+}
+
+window.onload = function () {
+    // if localStorage has the key 'cookie'
+    if (localStorage.getItem('cookie')) {
+        // hide the cookie alert with a fade out
+        document.getElementById('cookie').style.display = 'none';
+    }
+}
+
+function got_it(){
+    // set the key 'cookie' to localStorage
+    localStorage.setItem('cookie', 'true');
+    // hide the cookie alert with animation which will move the alert to the right
+    document.getElementById('cookie').style.transition = 'all 0.5s';
+    document.getElementById('cookie').style.transform = 'translateX(100%)';
 }
